ac6bc9c0463ec96d12d2be659a935fb6e623bf83
bvdheiden <b.vdh@protonmail.com>
2020-05-16 16:23:15 +0200

    Set-up module structure

---
+++ client/src/main/java/doodlejump/client/Client.java
@@ -0,0 +1,12 @@
+package doodlejump.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("DoodleJump client");
+        primaryStage.show();
+    }
+}
---
+++ server/src/main/java/doodlejump/server/Server.java
@@ -0,0 +1,12 @@
+package doodlejump.server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Server extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("DoodleJump server");
+        primaryStage.show();
+    }
+}
