d5f7213dd48bd75431253474fc431c48c1905238
bvdheiden <b.vdh@protonmail.com>
2020-06-02 21:01:20 +0200

    Add collider to Platform

--- client/src/main/java/doodlejump/client/game/Platform.java
+++ client/src/main/java/doodlejump/client/game/Platform.java
@@ -1,5 +1,9 @@
 package doodlejump.client.game;
 
+import doodlejump.client.game.collision.Vector2;
+import doodlejump.client.game.collision.colliders.BoxCollider;
+import doodlejump.client.game.collision.colliders.Collider2D;
+import doodlejump.client.game.collision.enums.ColliderTag;
 import org.jetbrains.annotations.Contract;
 
 public class Platform {
@@ -8,12 +12,18 @@ public class Platform {
     private final double x;
     private final double y;
     private final double width;
+    private final BoxCollider collider;
 
     @Contract(pure = true)
     public Platform(double x, double y, double width) {
         this.x = x;
         this.y = y;
         this.width = width;
+        this.collider = new BoxCollider(new Vector2(x, y), width, HEIGHT);
+
+        collider.collisionCallback = this::OnCollision;
+        collider.setColliderTag(ColliderTag.PLATFORM);
+        collider.setOwnerObject(this);
     }
 
     public double getX() {
@@ -30,5 +40,9 @@ public class Platform {
 
     public double getHeight() {
         return HEIGHT;
+    }
+
+    private void OnCollision(Collider2D collider2D) {
+        // do collision
     }
 }
